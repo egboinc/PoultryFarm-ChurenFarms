372634353a0ca69b1fd800e95bfe21cf966ce053
egboinc <Nduka.egbochuku@egboinc.com>
2022-07-12 02:12:26 -0400

    one more toniight

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { ChurenFarms});
+  res.render('index', { title: 'ChurenFarms' });
 });
 
 module.exports = router;
